d5ca2187e3efdad4b6c21d2e71d0e3f3714e72fd
email <ptaszol69@gmail.com>
2019-02-01 18:05:59 +0100

    Simple variables - int

---
+++ 01_SimpleVariables/Main.java
@@ -0,0 +1,18 @@
+package ptaszol69;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My first simple variables!!!");
+
+        int myFirstNumber = 13;
+        int mySecondNumber = 17;
+        int myThirdNumber = myFirstNumber * mySecondNumber;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLast = 1000 - myTotal;
+
+        System.out.println(myLast);
+    }
+}
