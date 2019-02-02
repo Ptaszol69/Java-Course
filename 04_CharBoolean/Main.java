d3b8b18eeff63331ab30337e9e3e101c58a8bdc8
Ptaszol69 <ptaszol69@gmail.com>
2019-02-02 11:32:04 +0100

    Variables char, boolean

---
+++ 04_CharBoolean/Main.java
@@ -0,0 +1,16 @@
+package com.ptaszol69;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // width of 16 (2 bytes)
+        char myRegChar = '\u00AE';
+        System.out.println(myRegChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char myCopyChar = '\u00A9';
+        System.out.println(myCopyChar);
+    }
+}
